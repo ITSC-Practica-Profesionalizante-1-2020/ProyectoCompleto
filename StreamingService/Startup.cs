24454160cf7ce63519adcae4600b867f8a92cf12
Ezequiel Monforte <ezequielmonforte@outlook.com>
2020-11-21 16:43:52 -0300

    merge changes

--- StreamingService/Startup.cs
+++ StreamingService/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CameraStream.Hubs;
+using CameraStream.Models;
 using DAL.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -27,10 +29,21 @@ namespace StreamingService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRazorPages().AddRazorRuntimeCompilation();
             services.AddHttpClient();
             services.AddControllers();
             services.AddDbContext<DataContext>(
            options => options.UseSqlServer(Configuration.GetConnectionString("Conexion")));
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
+
+            services.AddSignalR();
+
+            services.AddSingleton<List<User>>();
+            services.AddSingleton<List<Connection>>();
+            services.AddSingleton<List<Call>>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,16 +53,28 @@ namespace StreamingService
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCors("CorsPolicy");
 
             app.UseRouting();
 
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapRazorPages();
+                endpoints.MapHub<ConnectionHub>("/cnnctn", options =>
+                {
+                    options.Transports = Microsoft.AspNetCore.Http.Connections.HttpTransportType.WebSockets;
+                });
             });
         }
     }
